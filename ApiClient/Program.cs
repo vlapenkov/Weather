81a98f489af5da42ee6579be3d05277af10aaa64
Vladimir Lapenkov <vlapenkov@ya.ru>
2020-05-14 17:05:48 +0300

    init

---
+++ ApiClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace ApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string cs = @"server=localhost;userid=dbuser;password=s$cret;database=testdb";
+            string cs = @"server=db;userid=root;password=root;database=new_base";
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            using var cmd = new MySqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = "DROP TABLE IF EXISTS weather";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE weather(id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                    weatherdesc TEXT)";
+            cmd.ExecuteNonQuery();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                String weatherDesc = new WebClient().DownloadString("http://weather/weatherforecast");
+                //
+               //  cmd.CommandText = $"INSERT INTO weather(weatherdesc) VALUES({weatherDesc})";
+                cmd.CommandText = $"INSERT INTO weather(weatherdesc) VALUES('"+ weatherDesc + "')";
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ DBReader/Program.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Threading;
+
+namespace DBReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                //  string cs = @"server=localhost;userid=dbuser;password=s$cret;database=mydb";
+                string cs = @"server=db;userid=root;password=root;database=new_base";
+
+                using var con = new MySqlConnection(cs);
+                con.Open();
+
+                MySqlCommand command = con.CreateCommand();
+               
+                command.CommandText = "SELECT COUNT(*) FROM weather";
+               
+               
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                 
+                    var weather = command.ExecuteScalar().ToString();
+                    Console.WriteLine($"The weather is: {weather}");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
